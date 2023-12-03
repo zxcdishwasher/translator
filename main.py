2a649f2ced3df85809adf7fcb6a4d9836eddd2b6
zxcdishwasher <150586657+zxcdishwasher@users.noreply.github.com>
2023-12-03 11:58:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,77 @@
+import discord
+from discord.ext import commands
 import random
-import time
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
 
-eng_words = ['Hi','Bye','Task', 'Programm']
-ru_words = ['Привет','Пока','Задача', 'Программа']
-score = 0
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-mod = input("Выбери режим работы тренажера: 0 - добавить новые слова, 1 - тренироваться: \n")
-while ((mod != '0') and (mod != '1')):
-    mod = input("Недопустимый символ! Выбери 0 или 1. (0 - добавить новые слова, 1 - тренироваться) \n")
 
-if mod == "1":
-    print("Переведи как можно больше слов правильно! У тебя 10 попыток!")
-    for i in range(10):
-        number = random.randint(0, len(eng_words))
-        print("Как переводится слово: " + eng_words[number])
-        if input() == ru_words[number]:
-            print("Отлично!!!")
-            score += 1
-        else:
-            print("Нет... Это слово - " + ru_words[number])
-else:
-    word = input("Введите слово на русском языке: ")
-    translate = input("Введите перевод этого слова: ")
-    if len(word) > 0 and len(translate) > 0:
-        ru_words.append(word)
-        eng_words.append(translate)
-        print("Слово успешно добавлено!")
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """ты лубишь мороженное"""
+    await ctx.send('Да, я люблю мороженное')
+
+
+bot.run('MTE3ODI3MTM0NDYxODY1MTc1OQ.G10HZC.SxJlcCOmnnKNofgPKExvJ4PKT_AjS5u8FW3r7Q')
